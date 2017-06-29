875e52da5c1f3f405d0f3841be136ff45b7abc9c
hoffman250 <hoffman250@gmail.com>
2017-06-28 17:42:52 -0700

    added bootstrap template files base.html and 404.html

--- chap3/bootstrap.py
+++ chap3/bootstrap.py
@@ -16,6 +16,9 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run()
