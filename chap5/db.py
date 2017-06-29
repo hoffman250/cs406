57b7b83cc3cf444dfe178e66420aecaa84fdc36c
hoffman250 <hoffman250@gmail.com>
2017-06-29 12:41:25 -0700

    Added code to create sql database, added a few entries to db, modified index route to add names entered in form to database

--- chap5/db.py
+++ chap5/db.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_script import Manager
 from flask_moment import Moment 
 from datetime import datetime
 from flask_wtf import FlaskForm
@@ -17,6 +18,7 @@ app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'Andy LaRocque is a great guitarist' 
 
+manager = Manager(app)
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -29,7 +31,7 @@ class Role(db.Model):
 	__tablename__ = 'roles'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(64), unique=True)
-	users = db.relationship('User', backref='role')
+	users = db.relationship('User', backref='role', lazy='dynamic')
 
 	def __repr__(self):
 		return '<Role %r>' % self.name
@@ -38,6 +40,7 @@ class User(db.Model):
 	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), unique=True, index=True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 	def __repr__(self):
 		return '<User %r>' % self.username
@@ -46,13 +49,18 @@ class User(db.Model):
 def index():
 	form = NameForm()
 	if form.validate_on_submit():
-		old_name = session.get('name')
-		if old_name is not None and old_name != form.name.data:
-			flash('Looks like your name is changed.')
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user = User(username = form.name.data)
+			db.session.add(user)
+			session['known'] = False
+		else:
+			session['known'] = True
 		session['name'] = form.name.data
 		form.name.data = ''
 		return redirect(url_for('index'))
 	return render_template('index.html', form=form, name=session.get('name'), 
+		known = session.get('known', False),
 		current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
@@ -68,4 +76,5 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    app.run()
+	db.create_all()
+	manager.run()
