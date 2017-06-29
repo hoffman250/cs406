7af7a56929d78194240a705dbb6db83033d4e45e
hoffman250 <hoffman250@gmail.com>
2017-06-28 20:43:01 -0700

    added wtf code. added form template for index page.

--- chap4/forms.py
+++ chap4/forms.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.moment import Moment 
+from flask import Flask, render_template, session, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment 
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Andy LaRocque is a great guitarist' 
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(FlaskForm):
+	name = StringField('What is your name?', validators=[Required()])
+	submit=SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+	form = NameForm()
+	if form.validate_on_submit():
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', form=form, name=session.get('name'), 
+		current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
