4d2f5d97f9f4744606adbfc4da59f2dec0d84ea0
hoffman250 <hoffman250@gmail.com>
2017-07-06 11:57:17 -0700

    added errors.py and views.py to main dir to hold routes. updated create_app() factory function to use blueprint for accessing routes.

--- chap7/app/__init__.py
+++ chap7/app/__init__.py
@@ -20,42 +20,7 @@ def create_app(config_name):
 	moment.init_app(app)
 	db.init_app(app)
 
-	#routes
-	@app.route('/', methods=['GET', 'POST'])
-	def index():
-		form = NameForm()
-		if form.validate_on_submit():
-			user = User.query.filter_by(username=form.name.data).first()
-			if user is None:
-				user = User(username = form.name.data)
-				db.session.add(user)
-				session['known'] = False
-				if app.config['FLASKY_ADMIN']:
-					send_email(app.config['FLASKY_ADMIN'], 'New User',
-	            	'mail/new_user', user=user)
-			else:
-				session['known'] = True
-			session['name'] = form.name.data
-			form.name.data = ''
-			return redirect(url_for('index'))
-		return render_template('index.html', form=form, name=session.get('name'), 
-			known = session.get('known', False),
-			current_time=datetime.utcnow())
-
-	@app.route('/user/<name>')
-	def user(name):
-	    return render_template('user.html', name=name)
-
-	@app.errorhandler(404)
-	def page_not_found(e):
-	    return render_template('404.html'), 404
-
-	@app.errorhandler(500)
-	def internal_server_error(e):
-	    return render_template('500.html'), 500
-
-	if __name__ == '__main__':
-		# db.create_all()
-		manager.run()
+	from .main import main as main_blueprint
+	app.register_blueprint(main_blueprint)
 
 	return app 
