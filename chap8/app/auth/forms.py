750372cfc682b7d3adf0fc77d31e4baf2c0d9b83
hoffman250 <hoffman250@gmail.com>
2017-07-06 21:02:58 -0700

    Fixed syntax errors in login.html

--- chap8/app/auth/forms.py
+++ chap8/app/auth/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import Form
-from wtforms import Stringfield, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Email
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Required, Length, Email
 
 class LoginForm(Form):
-	email = Stringfield('Email', validators=[Required(), Length(1, 64), Email()])
+	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
 	password = PasswordField('Password', validators=[Required()])
 	remember_me = BooleanField('Keep me logged in')
 	submit = SubmitField('Log In')
