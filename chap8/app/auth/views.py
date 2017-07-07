2e7d3fe6039dcb364b670c2399ca9bcba77206a1
hoffman250 <hoffman250@gmail.com>
2017-07-07 15:15:02 -0700

    added code to send confirmation email when a user registers.

--- chap8/app/auth/views.py
+++ chap8/app/auth/views.py
@@ -6,6 +6,15 @@ from ..models import User
 from ..email import send_email
 from .forms import LoginForm, RegistrationForm
 
+@auth.before_app_request
+def before_request():
+	if current_user.is_authenticated \
+			and not current_user.confirmed \
+			and request.endpoint \
+			and request.endpoint[:5] != 'auth.' \
+			and request.endpoint != 'static':
+		return redirect(url_for('auth.unconfirmed'))
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
@@ -46,7 +55,22 @@ def confirm(token):
 	if current_user.confirmed:
 		return redirect(url_for('main.index'))
 	if current_user.confirm(token):
-		flash('You have confirmed your account. Dankeschön.')
+		flash('You have confirmed your account. Dankeschoen.')
 	else:
 		flash('The confirmation link is invalid or expired.')
 	return redirect(url_for('main.index'))
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+	token = current_user.generate_confirmation_token()
+	send_email(current_user.email, 'Confirm Your Account',
+		'auth/email/confirm', user=current_user, token=token)
+	flash('A new confirmation email has been sent to you')
+	return redirect(url_for('main.index'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+	if current_user.is_anonymous or current_user.confirmed:
+		return redirect('main.index')
+	return render_template('auth/unconfirmed.html')
