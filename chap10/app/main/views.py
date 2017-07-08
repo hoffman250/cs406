f37cc34cbf4dce02d8d158b33a0e0d28f5fb18d6
hoffman250 <hoffman250@gmail.com>
2017-07-08 15:10:42 -0700

    Added code for admin editing of profiles

--- chap10/app/main/views.py
+++ chap10/app/main/views.py
@@ -3,8 +3,8 @@ from flask import render_template, session, redirect, url_for, current_app, flas
 from .. import db
 from . import main
 from ..decorators import admin_required, permission_required
-from .forms import NameForm, EditProfileForm
-from ..models import User
+from .forms import NameForm, EditProfileForm, EditProfileAdminForm
+from ..models import Role, User
 from ..email import send_email
 from flask_login import login_required, current_user
 from ..models import Permission
@@ -45,3 +45,29 @@ def edit_profile():
 	form.location.data = current_user.location
 	form.about_me.data = current_user.about_me
 	return render_template('edit_profile.html', form=form)
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+	user = User.query.get_or_404(id)
+	form = EditProfileAdminForm(user=user)
+	if form.validate_on_submit():
+		user.email = form.email.data
+		user.username = form.username.data
+		user.confirmed = form.confirmed.data
+		user.role = Role.query.get(form.role.data)
+		user.name = form.name.data
+		user.location = form.location.data
+		user.about_me = form.about_me.data
+		db.session.add(user)
+		flash('User profile has been updated.')
+		return redirect(url_for('.user', username=user.username))
+	form.email.data = user.email
+	form.username.data = user.username
+	form.confirmed.data = user.confirmed
+	form.role.data = user.role
+	form.name.data = user.name
+	form.location.data = user.location
+	form.about_me.data = user.about_me
+	return render_template('edit_profile.html', form=form, user=user)
