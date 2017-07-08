66c0063fecb56b5ef37df8723e35d54f1797579f
hoffman250 <hoffman250@gmail.com>
2017-07-08 16:05:25 -0700

    added avatar code

--- chap10/app/models.py
+++ chap10/app/models.py
@@ -2,9 +2,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from . import login_manager
 from flask_login import UserMixin, AnonymousUserMixin
-from flask import current_app
+from flask import current_app, request
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from datetime import datetime
+import hashlib
 
 class Permission:
 	FOLLOW = 0X01
@@ -58,6 +59,7 @@ class User(UserMixin, db.Model):
 	about_me = db.Column(db.Text())
 	member_since = db.Column(db.DateTime(), default=datetime.utcnow)
 	last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+	avatar_hash = db.Column(db.String(32))
 
 	def __init__(self, **kwargs):
 		super(User, self).__init__(**kwargs)
@@ -66,6 +68,8 @@ class User(UserMixin, db.Model):
 				self.role = Role.query.filter_by(permissions=0xff).first()
 			if self.role is None:
 				self.role = Role.query.filter_by(default=True).first()
+		if self.email is not None and self.avatar_hash is None:
+			self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
 
 	@property 
 	def password(self):
@@ -94,6 +98,45 @@ class User(UserMixin, db.Model):
 		db.session.add(self)
 		return True
 
+	def generate_reset_token(self, expiration=3600):
+		s = Serializer(current_app.config['SECRET_KEY'], expiration)
+		return s.dumps({'reset': self.id})
+
+	def reset_password(self, token, new_password):
+		s = Serializer(current_app.config['SECRET_KEY'])
+		try:
+			data = s.loads(token)
+		except:
+			return False
+		if data.get('reset') != self.id:
+			return False
+		self.password = new_password
+		db.session.add(self)
+		return True
+
+	def generate_email_change_token(self, new_email, expiration=3600):
+		s = Serializer(current_app.config['SECRET_KEY'], expiration)
+		return s.dumps({'change_email': self.id, 'new_email': new_email})
+
+	def change_email(self, token):
+		s = Serializer(current_app.config['SECRET_KEY'])
+		try:
+			data = s.loads(token)
+		except:
+			return False
+		if data.get('change_email') != self.id:
+			return False
+		new_email = data.get('new_email')
+		if new_email is None:
+			return False
+		if self.query.filter_by(email=new_email).first() is not None:
+			return False
+		self.email = new_email
+		self.avatar_hash = hashlib.md5(
+			self.email.endcode('utf-8')).hexdigest()
+		db.session.add(self)
+		return True
+
 	def can(self, permissions):
 		return self.role is not None and \
 			(self.role.permissions & permissions) == permissions
@@ -104,6 +147,15 @@ class User(UserMixin, db.Model):
 	def ping(self):
 		self.last_seen = datetime.utcnow()
 		db.session.add(self)
+
+	def gravatar(self, size=100, default='identicon', rating='g'):
+		if request.is_secure:
+			url = 'https://secure.gravatar.com/avatar'
+		else:
+			url = 'http://secure.gravatar.com/avatar'
+		hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
+		return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+			url=url, hash=hash, size=size, default=default, rating=rating)
 
 	def __repr__(self):
 		return '<User %r>' % self.username
