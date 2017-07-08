c3e73c253130cd880ae35e86b7043b7ced6b1545
hoffman250 <hoffman250@gmail.com>
2017-07-07 17:28:32 -0700

    adding decorators.py

--- chap9/app/models.py
+++ chap9/app/models.py
@@ -9,10 +9,33 @@ class Role(db.Model):
 	__tablename__ = 'roles'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(64), unique=True)
+	default = db.Column(db.Boolean, default=False, index=True)
+	permissions = db.Column(db.Integer)
 	users = db.relationship('User', backref='role', lazy='dynamic')
 
 	def __repr__(self):
 		return '<Role %r>' % self.name
+
+	@staticmethod 
+	def insert_roles():
+		roles = {
+			'User': (Permission.FOLLOW |
+					  	Permission.COMMENT |
+						Permission.WRITE_ARTICLES, True),
+			'Moderator': (Permission.FOLLOW |
+					  		Permission.COMMENT |
+							Permission.WRITE_ARTICLES |
+							Permission.MODERATE_COMMENTS, False),
+			'Administrator': (0xff, False)
+		}
+		for r in roles:
+			role = Role.query.filter_by(name=r).first()
+			if role is None:
+				role = Role(name=r)
+			role.permissions = roles[r][0]
+			role.default = roles[r][1]
+			db.session.add(role)
+		db.session.commit()
 
 class User(UserMixin, db.Model):
 	__tablename__ = 'users'
@@ -57,3 +80,26 @@ class User(UserMixin, db.Model):
 		self.confirmed = True
 		db.session.add(self)
 		return True
+
+	def can(self, permissions):
+		return self.role is not None and \
+			(self.role.permissions & permissions) == permissions
+
+	def is_administrator(self):
+		return self.can(Permission.ADMINISTRATOR)
+
+class Permission:
+	FOLLOW = 0X01
+	COMMENT = 0X02
+	WRITE_ARTICLES = 0X04
+	MODERATE_COMMENTS = 0X08
+	ADMINISTRATOR = 0X80
+
+class AnonymousUser(AnonymousUserMixin):
+	def can(self, permissions):
+		return False
+
+	def is_administrator(self):
+		return False
+		
+login_manager.anonymous_user = AnonymousUser
