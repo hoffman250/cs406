2fb314e19f6c42867f1d64e50415e6012a1f3f4b
hoffman250 <hoffman250@gmail.com>
2017-07-07 20:48:15 -0700

    added code for profile information.

--- chap10/app/main/views.py
+++ chap10/app/main/views.py
@@ -9,9 +9,14 @@ from ..email import send_email
 from flask_login import login_required
 from ..models import Permission
 
-# @main.route('/')
-# def index():
-# 	return render_template('index.html')
+@main.route('/')
+def index():
+	return render_template('index.html')
+
+@main.route('/user/<username>')
+def user(username):
+	user = User.query.filter_by(username=username).first_or_404()
+	return render_template('user.html', user=user)
 
 @main.route('/admin')
 @login_required
@@ -25,24 +30,24 @@ def for_admins_only():
 def for_moderators_only():
 	return "For comment moderators!"
 
-@main.route('/', methods=['GET', 'POST'])
-def index():
-	form = NameForm()
-	if form.validate_on_submit():
-		user = User.query.filter_by(username=form.name.data).first()
-		if user is None:
-			user = User(username = form.name.data)
-			db.session.add(user)
-			session['known'] = False
-			if current_app.config['FLASKY_ADMIN']:
-				send_email(current_app.config['FLASKY_ADMIN'], 'New User',
-            	'mail/new_user', user=user)
-		else:
-			session['known'] = True
-		session['name'] = form.name.data
-		form.name.data = ''
-		return redirect(url_for('.index'))
-	return render_template('index.html', form=form, name=session.get('name'), 
-		known = session.get('known', False),
-		current_time=datetime.utcnow())
+# @main.route('/', methods=['GET', 'POST'])
+# def index():
+# 	form = NameForm()
+# 	if form.validate_on_submit():
+# 		user = User.query.filter_by(username=form.name.data).first()
+# 		if user is None:
+# 			user = User(username = form.name.data)
+# 			db.session.add(user)
+# 			session['known'] = False
+# 			if current_app.config['FLASKY_ADMIN']:
+# 				send_email(current_app.config['FLASKY_ADMIN'], 'New User',
+#             	'mail/new_user', user=user)
+# 		else:
+# 			session['known'] = True
+# 		session['name'] = form.name.data
+# 		form.name.data = ''
+# 		return redirect(url_for('.index'))
+# 	return render_template('index.html', form=form, name=session.get('name'), 
+# 		known = session.get('known', False),
+# 		current_time=datetime.utcnow())
 
