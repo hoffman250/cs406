7a6696fb05c53552e78b86a6218114aed4e7c557
hoffman250 <hoffman250@gmail.com>
2017-07-08 14:08:53 -0700

    added code for users to edit profile.

--- chap10/app/main/views.py
+++ chap10/app/main/views.py
@@ -1,12 +1,12 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, current_app
+from flask import render_template, session, redirect, url_for, current_app, flash
 from .. import db
 from . import main
 from ..decorators import admin_required, permission_required
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
 from ..models import User
 from ..email import send_email
-from flask_login import login_required
+from flask_login import login_required, current_user
 from ..models import Permission
 
 @main.route('/')
@@ -30,24 +30,18 @@ def for_admins_only():
 def for_moderators_only():
 	return "For comment moderators!"
 
-# @main.route('/', methods=['GET', 'POST'])
-# def index():
-# 	form = NameForm()
-# 	if form.validate_on_submit():
-# 		user = User.query.filter_by(username=form.name.data).first()
-# 		if user is None:
-# 			user = User(username = form.name.data)
-# 			db.session.add(user)
-# 			session['known'] = False
-# 			if current_app.config['FLASKY_ADMIN']:
-# 				send_email(current_app.config['FLASKY_ADMIN'], 'New User',
-#             	'mail/new_user', user=user)
-# 		else:
-# 			session['known'] = True
-# 		session['name'] = form.name.data
-# 		form.name.data = ''
-# 		return redirect(url_for('.index'))
-# 	return render_template('index.html', form=form, name=session.get('name'), 
-# 		known = session.get('known', False),
-# 		current_time=datetime.utcnow())
-
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+	form = EditProfileForm()
+	if form.validate_on_submit():
+		current_user.name = form.name.data
+		current_user.location = form.location.data
+		current_user.about_me = form.about_me.data
+		db.session.add(current_user)
+		flash('Your profile has been updated.')
+		return redirect(url_for('.user', username=current_user.username))
+	form.name.data = current_user.name
+	form.location.data = current_user.location
+	form.about_me.data = current_user.about_me
+	return render_template('edit_profile.html', form=form)
