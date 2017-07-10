9b2fc55d721a7a0d98c542999313d80c9d16b778
hoffman250 <hoffman250@gmail.com>
2017-07-09 21:48:50 -0700

    added code to support pagination of the blog posts

--- chap11/app/__init__.py
+++ chap11/app/__init__.py
@@ -5,6 +5,7 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config
+from flask_pagedown import PageDown
 
 bootstrap = Bootstrap()
 mail = Mail()
@@ -13,6 +14,7 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+pagedown = PageDown()
 
 def create_app(config_name):
 	app = Flask(__name__)
