b9449c2b592dd4b33edface252b0f0d997cc2a40
hoffman250 <hoffman250@gmail.com>
2017-07-10 23:18:23 -0700

    Added code to show followed posts on the home page

--- chap12/app/main/views.py
+++ chap12/app/main/views.py
@@ -15,6 +15,13 @@ def index():
 		db.session.add(post)
 		return redirect(url_for('.index'))
 	page = request.args.get('page', 1, type=int)
+	show_followed = False
+	if current_user.is_authenticated:
+		show_followed = bool(request.cookies.get('show_followed', ''))
+	if show_followed:
+		query = current_user.followed_posts
+	else:
+		query = Post.query
 	pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
 		page, per_page=current_app.config['POSTS_PER_PAGE'], 
 		error_out=False)
@@ -164,3 +171,17 @@ def followed_by(username):
 	return render_template('follwers.html', user=user, title="Followed by",
 							endpoint='.followed_by', pagination=pagination,
 							follows=follows)
+
+@main.route('/all')
+@login_required
+def show_all():
+	resp = make_response(redirect(url_for('.index')))
+	resp.set_cookie('show_followed', '', max_age=30*24*60*60)
+	return resp
+
+@main.route('/followed')
+@login_required
+def show_followed():
+	resp = make_response(redirect(url_for('.index')))
+	resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
+	return resp
