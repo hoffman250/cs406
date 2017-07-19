b967811935e8fe52f9712314e02e84d7f0bed49d
hoffman250 <hoffman250@gmail.com>
2017-07-18 21:19:03 -0700

    added purchase.html, renders form for user to input purchase information

--- bike_app_v01/app/main/forms.py
+++ bike_app_v01/app/main/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField 
-from wtforms.validators import Required, Length, Email, Regexp
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 from flask_pagedown.fields import PageDownField
 
 
-class PostForm(FlaskForm):
-	body = PageDownField("What you thinkin?", validators=[Required()])
-	submit = SubmitField("Submit")
+class PurchaseForm(FlaskForm):
+	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+	submit = SubmitField('Purchase')
--- bike_app_v01/app/main/views.py
+++ bike_app_v01/app/main/views.py
@@ -1,30 +1,30 @@
 from flask import render_template, redirect, url_for, flash, request, \
 	current_app, make_response
 from . import main
-
+from .forms import PurchaseForm
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-	
 	return render_template('index.html')
 
 @main.route('/about', methods=['GET', 'POST'])
 def about():
-	
 	return render_template('about.html')
 
 @main.route('/browse_bikes', methods=['GET', 'POST'])
 def browse_bikes():
-	
 	return render_template('browse_bikes.html')
 
 
 @main.route('/participating_dealers', methods=['GET', 'POST'])
 def participating_dealers():
-	
 	return render_template('participating_dealers.html')
 
 @main.route('/faq', methods=['GET', 'POST'])
 def faq():
-	
 	return render_template('faq.html')
+
+@main.route('/purchase', methods=['GET', 'POST'])
+def purchase():
+	form = PurchaseForm()
+	return render_template('purchase.html', form=form)
