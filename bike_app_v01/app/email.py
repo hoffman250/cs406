7bce47a424fb62373b9f284ee64e19c14bcfe5e7
hoffman250 <hoffman250@gmail.com>
2017-08-10 14:12:51 -0700

    Commented models.py and email.py

--- bike_app_v01/app/email.py
+++ bike_app_v01/app/email.py
@@ -1,12 +1,19 @@
+# email.py
+# file contains functions to send email
+# email functions courtesy of Flask Web Developement, Miguel Grinberg
+
 from threading import Thread
 from flask import current_app, render_template
 from flask_mail import Message
 from . import mail
 
+# moves email to background thread to prevent blocking
 def send_async_email(app, msg):
    	with app.app_context():
    	    mail.send(msg)
 
+# send email function
+# takes send to address, subject, email template, & keyword argurments
 def send_email(to, subject, template, **kwargs):
 	app = current_app._get_current_object()
 	msg = Message(app.config['MAIL_SUBJECT_PREFIX'] + ' ' + subject, 
--- bike_app_v01/app/models.py
+++ bike_app_v01/app/models.py
@@ -1,3 +1,7 @@
+# models.py
+# file contains models for User, Bike and Purchase tables in the database
+# File structure based on model.py in Flask Web Developement, Miguel Grinberg
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from . import login_manager
@@ -9,12 +13,14 @@ from markdown import markdown
 import bleach
 import hashlib
 
+# User table
+# creates table to hold user attributes
 class User(UserMixin, db.Model):
 	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key=True)
 	email = db.Column(db.String(64), unique=True, index=True)
 	username = db.Column(db.String(64), unique=True, index=True)
-	password_hash = db.Column(db.String(128))
+	password_hash = db.Column(db.String(128))	# stores password hash
 	confirmed = db.Column(db.Boolean, default=False)
 	first_name = db.Column(db.String(64))
 	last_name = db.Column(db.String(64))
@@ -29,40 +35,29 @@ class User(UserMixin, db.Model):
 	style = db.Column(db.String(64))
 	gender = db.Column(db.String(64))
 
-	@staticmethod     
-	def generate_fake(count=100):
-		from sqlalchemy.exc import IntegrityError
-		from random import seed
-		import forgery_py
-
-		seed()
-		for i in range(count):
-			u = User(email=forgery_py.internet.email_address(),
-					username=forgery_py.internet.user_name(True),
-					password=forgery_py.lorem_ipsum.word(),
-					confirmed=True,
-					name=forgery_py.name.full_name())
-			db.session.add(u)
-			try:
-				db.session.commit()
-			except IntegrityError:
-				db.session.rollback()
-
+	# returns error is hashed password is read
 	@property 
 	def password(self):
 		raise AttributeError('password is not a readable attribute')
 
+	# password generate function from Werkzeug
 	@password.setter
 	def password(self, password):
 		self.password_hash = generate_password_hash(password)
-		
+	
+	# Werkzeug function checks entered password against stored hashed password in User table
 	def verify_password(self, password):
 		return check_password_hash(self.password_hash, password)
 
+	# itsdangerous function generates confirmation token for registration
+	# uses secret key constant from config.py
+	# expires in 3600 seconds
 	def generate_confirmation_token(self, expiration=3600):
 		s = Serializer(current_app.config['SECRET_KEY'], expiration)
 		return s.dumps({'confirm': self.id})
 
+	# itsdangerous function verifies token
+	# if verified, sets 'confirmed' to true indicating user is authenticated
 	def confirm(self, token):
 		s = Serializer(current_app.config['SECRET_KEY'])
 		try:
@@ -75,57 +70,16 @@ class User(UserMixin, db.Model):
 		db.session.add(self)
 		return True
 
-	def generate_reset_token(self, expiration=3600):
-		s = Serializer(current_app.config['SECRET_KEY'], expiration)
-		return s.dumps({'reset': self.id})
-
-	def reset_password(self, token, new_password):
-		s = Serializer(current_app.config['SECRET_KEY'])
-		try:
-			data = s.loads(token)
-		except:
-			return False
-		if data.get('reset') != self.id:
-			return False
-		self.password = new_password
-		db.session.add(self)
-		return True
-
-	def generate_email_change_token(self, new_email, expiration=3600):
-		s = Serializer(current_app.config['SECRET_KEY'], expiration)
-		return s.dumps({'change_email': self.id, 'new_email': new_email})
-
-	def change_email(self, token):
-		s = Serializer(current_app.config['SECRET_KEY'])
-		try:
-			data = s.loads(token)
-		except:
-			return False
-		if data.get('change_email') != self.id:
-			return False
-		new_email = data.get('new_email')
-		if new_email is None:
-			return False
-		if self.query.filter_by(email=new_email).first() is not None:
-			return False
-		self.email = new_email
-		self.avatar_hash = hashlib.md5(
-			self.email.endcode('utf-8')).hexdigest()
-		db.session.add(self)
-		return True
-
+	# ping function used in auth/views.py, checks user confirmation status
 	def ping(self):
 		self.last_seen = datetime.utcnow()
 		db.session.add(self)
 
-	def __repr__(self):
-		return '<User %r>' % self.username
+	# def __repr__(self):
+	# 	return '<User %r>' % self.username
 
-class AnonymousUser(AnonymousUserMixin):
-	def can(self, permissions):
-		return False
-
-
+# Bike table
+# creates table to hold bike attributes
 class Bike(UserMixin, db.Model):
 	__tablename__ = 'bikes'
 	id = db.Column(db.Integer, primary_key=True)
@@ -134,7 +88,8 @@ class Bike(UserMixin, db.Model):
 	style = db.Column(db.String(64))
 	rate = db.Column(db.String(64))
 
-
+# Purchase table
+# creates table to hold purchase transaction attributes
 class Purchase(UserMixin, db.Model):
 	__tablename__ = 'purchases'
 	id = db.Column(db.Integer, primary_key=True)
@@ -142,6 +97,9 @@ class Purchase(UserMixin, db.Model):
 	credit_card_number = db.Column(db.String(64), index=True)
 	ccv = db.Column(db.String(64))
 
+class AnonymousUser(AnonymousUserMixin):
+	def can(self, permissions):
+		return False
 
 login_manager.anonymous_user = AnonymousUser
 
